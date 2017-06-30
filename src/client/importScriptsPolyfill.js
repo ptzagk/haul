210266694797583029e241321699d13aec216830
Paweł Trysła <pawel.trysla@gmail.com>
2017-06-30 09:49:16 +0200

    final touches in importScriptsPolyfill and hotClient

--- src/client/importScriptsPolyfill.js
+++ src/client/importScriptsPolyfill.js
@@ -10,40 +10,46 @@
  * When setting `target` to `webworker` in webpack config, it will change template for downloading
  * hot update and will use `importScripts` which are available in WebWorkers, so we need to
  * provide implementation for it.
+ * 
+ * Native `importScripts` is synchronous, however we can't do that, so this polyfill
+ * is async and returns a Promise.
  */
 global.importScripts = global.importScripts ||
-  (importPath => {
-    const timeout = 10000;
-    if (typeof XMLHttpRequest === 'undefined') {
-      throw new Error('No XMLHttpRequest support');
-    }
-
-    const request = new XMLHttpRequest();
-    request.timeout = timeout;
-    request.onreadystatechange = () => {
-      if (request.readyState !== 4) {
-        return;
+  (importPath =>
+    new Promise((resolve: Function, reject: Function) => {
+      const timeout: number = 10000;
+      if (typeof XMLHttpRequest === 'undefined') {
+        reject(new Error('No XMLHttpRequest support'));
       }
 
-      if (request.status === 0) {
-        // Timeout
-        throw new Error(`Request for ${importPath} timed out`);
-      } else if (request.status === 404) {
-        throw new Error(`Resource ${importPath} was not found on server`);
-      } else if (request.status !== 200 && request.status !== 304) {
-        // Unknown error
-        throw new Error(`Request for ${importPath} failed`);
-      } else {
-        // Apply changes from hot update
-        // eslint-disable-next-line no-eval
-        eval(request.responseText);
-      }
-    };
+      const request = new XMLHttpRequest();
+      request.timeout = timeout;
+      request.onreadystatechange = () => {
+        if (request.readyState !== 4) {
+          resolve();
+          return;
+        }
 
-    try {
-      request.open('GET', importPath);
-    } catch (e) {
-      // NOOP
-    }
-    request.send(null);
-  });
+        if (request.status === 0) {
+          // Timeout
+          reject(new Error(`Request for ${importPath} timed out`));
+        } else if (request.status === 404) {
+          reject(new Error(`Resource ${importPath} was not found on server`));
+        } else if (request.status !== 200 && request.status !== 304) {
+          // Unknown error
+          reject(new Error(`Request for ${importPath} failed`));
+        } else {
+          // Apply changes from hot update
+          // eslint-disable-next-line no-eval
+          eval(request.responseText);
+          resolve();
+        }
+      };
+
+      try {
+        request.open('GET', importPath);
+        request.send(null);
+      } catch (e) {
+        reject(e);
+      }
+    }));
